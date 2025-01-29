cf0ac9bfbd5b86dad98365c71977d5fe1a488dfb
Priyanka Gaikwad <priyankagaikwad.nbnssoe.comp@gmail.com>
2025-01-29 12:08:31 -0800

    datatype_summary

---
+++ datatypes_summary.js
@@ -0,0 +1,47 @@
+// primitive 
+
+// 7 types : String , Number , Boolean , null , undefined , Symbol , BigInt
+
+const score = 100 
+const scoreVlaue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 34567898765432123456789n
+// console.log(bigNumber);
+// console.log(typeof bigNumber);
+
+
+
+
+// Reference (Non - Primitive)
+
+// 3 Types : Array , Objects , functions
+
+// Array 
+const Heros = ["shaktiman" , "naagraj" , "doga"]
+
+// Objects 
+let myObj = {
+    name : "Priyanka",
+    age : 22
+}
+
+// functions 
+
+const myFunction = function()
+{
+    console.log("Jay Ganesh");
+    
+}
+console.log(typeof myFunction);
+console.log(typeof myObj);
+
