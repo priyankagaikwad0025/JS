fea34239ea5ff88c75bc8ff569cc190cfca4e459
Priyanka Gaikwad <priyankagaikwad.nbnssoe.comp@gmail.com>
2025-01-28 20:24:47 -0800

    conversion tutorial

---
+++ 03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33"
+console.log(score,typeof score)
+
+let valueInNumber = Number(score)
+console.log(score,typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 
+// false => 0
+
+let isLoggedIn= 1 ;;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let iNum = 33 
+let StringNumber = String(iNum)
+console.log(typeof StringNumber);
