bc9597871d4ed0608196f25b489a442419704aec
Priyanka Gaikwad <priyankagaikwad.nbnssoe.comp@gmail.com>
2025-01-29 11:48:58 -0800

    new files

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,9 +1,9 @@
 let score = "33"
-console.log(score,typeof score)
+// console.log(score,typeof score)
 
 let valueInNumber = Number(score)
-console.log(score,typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(score,typeof valueInNumber)
+// console.log(valueInNumber)
 
 // "33" => 33
 // "33abc" => NaN
@@ -12,8 +12,53 @@ console.log(valueInNumber)
 
 let isLoggedIn= 1 ;;
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 let iNum = 33 
 let StringNumber = String(iNum)
-console.log(typeof StringNumber);
+// console.log(typeof StringNumber);
+
+
+// operations
+
+let value = 3 
+let negValue = -value
+// console.log(negValue)
+
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2)
+console.log(2%2)
+*/
+
+let str1 = "hello "
+let str2 = "Priyanka"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" +2);
+// console.log(1+"2");
+// console.log("1" +  "2")
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+);
+// console.log(+"");
+
+let num1 , num2 , num3
+
+num1 =  num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+++gameCounter ;
+console.log(gameCounter);
+
+
